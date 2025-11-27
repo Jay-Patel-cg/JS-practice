416b8f60dcd9263ae7bfbf9d5b3dd8fbebd0883c
Jay-Patel-cg <jay.patel.a.cg@gmail.com>
2025-11-27 10:53:37 +0530

    LCM Question Two Diff Aprocee

--- 27-11-2025/30.js
+++ 27-11-2025/30.js
@@ -1,2 +1,30 @@
 //  LCM of Two Numbers (least common multiple). (લ.સા.અ.)
 
+let a = parseInt(prompt("Enter first number: "));
+let b = parseInt(prompt("Enter second number: "));
+
+let max = (a > b) ? a : b;
+let lcm = max;
+
+while (true) {
+  if (lcm % a == 0 && lcm % b == 0) {
+    break;   
+  }
+  lcm++;     
+}
+
+console.log("LCM = " + lcm);
+
+
+//  Anubhav-sir Apporce
+
+let f = 20;  
+let s = 44;
+ let largest = Math.max(f,s);
+
+ for(let i = largest; i<=f*s; i++){
+  if(i%f==0 && i%s==0){
+    console.log(i);
+    return;
+  }
+ }
